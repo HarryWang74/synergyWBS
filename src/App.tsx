842d3c2b0e64ee7c00eb66249f1006a216e93b10
Gang Wang <gang.wang@totalsynergy.com>
2025-04-30 15:43:25 +1000

    remove unnecessary css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { StrictMode, useState } from 'react'
-import { createRoot } from 'react-dom/client'
+import { useState } from 'react'
 
 // Theme
 import type { ColDef } from 'ag-grid-community'
